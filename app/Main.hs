66638c392c5dbc0cca27a0a5e7f60af40a604645
Joe Moore <joverandout@gmail.com>
2023-12-11 19:19:26 +0000

    Day 2 Part 1 - Parsing was hard the rest was easy

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 module Main (main) where
 
 import Day01
+import Day02
 import Lib
+import Text.Regex.Applicative (Alternative (many), RE, string, sym, (<|>), (=~))
 
 main :: IO ()
 main = undefined
---
+++ src/Day02.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE ApplicativeDo #-}
+
+module Day02 (module Day02) where
+
+import Data.Bits (Bits (xor))
+import Data.Map (Map ())
+import qualified Data.Map.Strict as Map
+import Data.Maybe (mapMaybe)
+import Text.Regex.Applicative (Alternative (many), RE, string, sym, (<|>), (=~))
+import Text.Regex.Applicative.Common (decimal)
+
+day2 :: IO ()
+day2 = readFile "inputs/day02.txt" >>= print . part1 . mapMaybe (=~ parseGame) . lines
+
+data Colour = Red | Green | Blue deriving (Show, Eq, Ord)
+
+data Pick = Pick Int Colour deriving (Show)
+
+data Pull = Pull [Pick] deriving (Show)
+
+data PickKVP = PickKVP (Colour, Int) deriving (Show)
+
+data Game = Game {id :: Int, pulls :: [Pull]} deriving (Show)
+
+seperated :: Alternative f => f a -> f b -> f [a]
+seperated p filler = (:) <$> p <*> many (filler *> p)
+
+parseColour :: RE Char Colour
+parseColour =
+  Red <$ string "red"
+    <|> Blue <$ string "blue"
+    <|> Green <$ string "green"
+
+parsePick :: RE Char Pick
+parsePick = Pick <$> decimal <* sym ' ' <*> parseColour
+
+parsePull :: RE Char Pull
+parsePull = Pull <$> seperated parsePick (string ", ")
+
+parsePulls :: RE Char [Pull]
+parsePulls = seperated parsePull $ string "; "
+
+parseGame :: RE Char Game
+parseGame = Game <$> (string "Game " *> decimal) <* string ": " <*> parsePulls
+
+pickToKVP :: Pick -> (Colour, Int)
+pickToKVP (Pick n c) = (c, n)
+
+picksToKVPs :: [Pick] -> [(Colour, Int)]
+picksToKVPs = foldr (\x -> (++) [pickToKVP x]) []
+
+pullToMap :: Pull -> Map Colour Int
+pullToMap (Pull picks) = Map.fromList (picksToKVPs picks)
+
+pullsToMap :: [Pull] -> Map Colour Int
+pullsToMap = foldr (Map.unionWith max . pullToMap) Map.empty
+
+gameToMap :: Game -> Map Colour Int
+gameToMap (Game _ pulls) = pullsToMap pulls
+
+isGameEligible :: Game -> Bool
+isGameEligible game =
+  getCountFromMaybe (Map.lookup Blue map) <= 14
+    && getCountFromMaybe (Map.lookup Green map) <= 13
+    && getCountFromMaybe (Map.lookup Red map) <= 12
+  where
+    map = gameToMap game
+
+getCountFromMaybe :: Maybe Int -> Int
+getCountFromMaybe (Just x) = x
+getCountFromMaybe Nothing = 0
+
+sumGames :: [Game] -> Int
+sumGames [] = 0
+sumGames ((Game id _) : xs) = id + sumGames xs
+
+part1 :: [Game] -> Int
+part1 games = sumGames (filter isGameEligible games)
