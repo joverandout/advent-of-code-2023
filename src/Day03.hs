753ef71643da45ac544c0e53dec27e13059033d0
Joe Moore <joverandout@gmail.com>
2023-12-16 19:29:20 +0000

    Day 3 Part 2 - Couldn't get it to work as a hashmap so it's VERY inefficient

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TupleSections #-}
 
 module Day03 (module Day03) where
 
+import Control.Arrow
 import Data.Char (isDigit)
-import Data.List (nub)
+import Data.List (intersect, nub)
 import Data.Map (Map (), member)
 import qualified Data.Map.Strict as Map
 
@@ -13,7 +15,7 @@ data Coordinate = Coordinate {x, y :: Int} deriving (Show, Eq, Ord)
 data Input = Map Coordinate Bool deriving (Show)
 
 day3 :: IO ()
-day3 = readFile "inputs/day03.txt" >>= print . part1 . lines
+day3 = readFile "inputs/day03.txt" >>= print . (part1 &&& part2) . lines
 
 parseTokensFromLine :: Int -> Int -> String -> [(Coordinate, Bool)]
 parseTokensFromLine _ _ [] = []
@@ -21,9 +23,19 @@ parseTokensFromLine y pos (x : xs)
   | x == '.' || isDigit x = parseTokensFromLine y (pos + 1) xs
   | otherwise = (Coordinate pos y, True) : parseTokensFromLine y (pos + 1) xs
 
+parseGearsFromLine :: Int -> Int -> String -> [Coordinate]
+parseGearsFromLine _ _ [] = []
+parseGearsFromLine y pos (x : xs)
+  | x /= '*' = parseGearsFromLine y (pos + 1) xs
+  | otherwise = Coordinate pos y : parseGearsFromLine y (pos + 1) xs
+
 parseTokensFromLines :: [String] -> Int -> [(Coordinate, Bool)]
 parseTokensFromLines [] _ = []
 parseTokensFromLines (x : xs) y = parseTokensFromLine y 1 x ++ parseTokensFromLines xs (y + 1)
+
+parseGearsFromLines :: [String] -> Int -> [Coordinate]
+parseGearsFromLines [] _ = []
+parseGearsFromLines (x : xs) y = parseGearsFromLine y 1 x ++ parseGearsFromLines xs (y + 1)
 
 parseTokensToMap :: [String] -> Map Coordinate Bool
 parseTokensToMap s = Map.fromList $ parseTokensFromLines s 1
@@ -43,12 +55,12 @@ sumValidNums num neighbours total tokens
 coordsInMap :: [Coordinate] -> Map Coordinate Bool -> Bool
 coordsInMap cs map = foldr (\c -> (||) (member c map)) False cs
 
-extractGridCoordinates :: [String] -> Int -> [(Int, [Coordinate])]
-extractGridCoordinates [] _ = []
-extractGridCoordinates (x : xs) y = extractRowCoordinates x y ++ extractGridCoordinates xs (y + 1)
+extractGridCoordinates :: [String] -> Int -> (Int -> Int -> Int -> [Coordinate]) -> [(Int, [Coordinate])]
+extractGridCoordinates [] _ _ = []
+extractGridCoordinates (x : xs) y getCoords = extractRowCoordinates x y getCoords ++ extractGridCoordinates xs (y + 1) getCoords
 
-extractRowCoordinates :: String -> Int -> [(Int, [Coordinate])]
-extractRowCoordinates s yPos = extraction s yPos 1 []
+extractRowCoordinates :: String -> Int -> (Int -> Int -> Int -> [Coordinate]) -> [(Int, [Coordinate])]
+extractRowCoordinates s yPos getCoords = extraction s yPos 1 []
   where
     extraction [] _ _ numsAndPos = numsAndPos
     extraction (c : cs) yPos xPos numsAndPos
@@ -65,19 +77,39 @@ extractRowCoordinates s yPos = extraction s yPos 1 []
              in extraction rest yPos (newPos + 1) (newList num)
       | otherwise = extraction cs yPos (xPos + 1) numsAndPos
       where
-        newList num = numsAndPos ++ [(num, getCoordinates num xPos yPos)]
+        newList num = numsAndPos ++ [(num, getCoords num xPos yPos)]
+
+getNeighbouringCoordinates :: Int -> Int -> Int -> [Coordinate]
+getNeighbouringCoordinates num xPos yPos = getNeighboursFromRange [Coordinate x yPos | x <- [xPos .. xPos + length (show num) - 1]]
 
 getCoordinates :: Int -> Int -> Int -> [Coordinate]
-getCoordinates num xPos yPos = getNeighboursFromRange [Coordinate x yPos | x <- [xPos .. xPos + length (show num) - 1]]
-
-integerBordersSymbol :: [Coordinate] -> Map Coordinate Bool -> Bool
-integerBordersSymbol xs hashMap = foldr (\x y -> member x hashMap || y) False xs
+getCoordinates num xPos yPos = [Coordinate x yPos | x <- [xPos .. xPos + length (show num) - 1]]
 
 addUpNumbersBorderingSymbol :: [(Int, [Coordinate])] -> Map Coordinate Bool -> Int -> Int
 addUpNumbersBorderingSymbol [] _ total = total
 addUpNumbersBorderingSymbol ((int, coords) : xs) symMap total
-  | integerBordersSymbol coords symMap = addUpNumbersBorderingSymbol xs symMap (total + int)
+  | coordsInMap coords symMap = addUpNumbersBorderingSymbol xs symMap (total + int)
   | otherwise = addUpNumbersBorderingSymbol xs symMap total
 
+numbersNextToGear :: Coordinate -> [(Int, [Coordinate])] -> [Int] -> [Int]
+numbersNextToGear _ [] ints = ints
+numbersNextToGear gear ((int, coords) : xs) ints
+  | not (null (getNeighbours gear `intersect` coords)) = int : numbersNextToGear gear xs ints
+  | otherwise = numbersNextToGear gear xs ints
+
+getGearRatio :: Coordinate -> [(Int, [Coordinate])] -> Int
+getGearRatio gear parts = case neighbourParts of
+  [x, y] -> x * y
+  _ -> 0
+  where
+    neighbourParts = numbersNextToGear gear parts []
+
+getGearRatioSum :: [Coordinate] -> [(Int, [Coordinate])] -> Int
+getGearRatioSum [] _ = 0
+getGearRatioSum (gear : gears) parts = getGearRatio gear parts + getGearRatioSum gears parts
+
 part1 :: [String] -> Int
-part1 xs = addUpNumbersBorderingSymbol (extractGridCoordinates xs 1) (parseTokensToMap xs) 0
+part1 xs = addUpNumbersBorderingSymbol (extractGridCoordinates xs 1 getNeighbouringCoordinates) (parseTokensToMap xs) 0
+
+part2 :: [String] -> Int
+part2 xs = getGearRatioSum (parseGearsFromLines xs 1) (extractGridCoordinates xs 1 getCoordinates)
